7506a4955680c854fff0c3f4c20ceead733e9cc3
Johnsondev19 <onasanyagbenga1963@gmail.com>
2023-12-01 03:59:44 +0000

    j

--- script.js
+++ script.js
@@ -5,10 +5,4 @@ hamburger.addEventListener('click', () =>{
   hamburger.classList.toggle('active');
   header.classList.toggle('active') ;
 })
-
-document.querySelectorAll('').forEach(n => n.
-    addEventListener('click', () => {
-      hamburger.classList.remove('active') ;
-      header.classList.remove('active') ;
-    }))
 //
